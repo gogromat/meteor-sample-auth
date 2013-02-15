873538c70612fd813f173123d69d79caaac1a33e
gogromat <gogromat@gmail.com>
2013-02-15 04:49:33 -0500

    Added allow/deny methods (for client only) and Meteor.method calls (for everyone)

--- client/simple_auth.js
+++ client/simple_auth.js
@@ -25,6 +25,20 @@ Template.lists.events({
     var new_name = evt.target.value,
         id = this._id;
     console.log("New name is:",new_name);
+    if (!Meteor.userId()) {
+      return;
+    }
     Lists.update(id,{$set : {list_name : new_name}});
+    return false;
+  }
+})
+
+Template.add_list.events({
+  'click #new_list_button': function(evt) {
+    var new_list = $('#new_list_name'),
+        new_list_name = new_list.val().trim();
+    console.log(evt.target.value,new_list_name);
+    Meteor.call("add_list",new_list_name);
+    new_list.val("");
   }
 })
